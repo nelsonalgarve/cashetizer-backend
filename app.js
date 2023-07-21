8b4f283676530ff52c83c9d85909caffa5119aa7
Nelson <nelson.guerreiro@nc-services.fr>
2023-07-21 22:51:38 +0200

    setup

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+require("./models/connection");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+const userRouter = require("./routers/user");
+const itemRouter = require("./routers/item");
+const cartRouter = require("./routers/cart");
+// const orderRouter = require("./routers/order");
+
+var app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(userRouter);
+app.use(userRouter);
+app.use(itemRouter);
+app.use(cartRouter);
+// app.use(orderRouter);
+module.exports = app;
