1976d7016106d66d3d2c047ac3f7fb017e4223c4
Mounir <Kasbaoui_mounir@outlook.com>
2023-08-02 10:14:41 +0200

    kl

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
 require('./models/connection');
-var express = require('express');
+const express = require('express');
+const fileUpload = require('express-fileupload');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 const userRouter = require('./routers/user');
+const checkIDRouter = require('./routers/checkID');
+const testUploadRouter = require('./routers/checkID');
 const itemRouter = require('./routers/item');
 const cartRouter = require('./routers/cart');
 const categoryRouter = require('./routers/category');
 // const orderRouter = require("./routers/order");
 
-var app = express();
+const fs = require('fs');
+if (!fs.existsSync('./tmp')) {
+    fs.mkdirSync('./tmp');
+  }
+
+const app = express();
+app.use(fileUpload());
 const cors = require('cors');
 app.use(cors());
 
@@ -23,7 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(userRouter);
 
 app.use(itemRouter);
+app.use(testUploadRouter);
 app.use(cartRouter);
+app.use(checkIDRouter);
 app.use(categoryRouter);
 // app.use(orderRouter);
 module.exports = app;
