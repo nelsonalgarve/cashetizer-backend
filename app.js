80d5b406edd925426b11df5a20e033dddaa0a27c
Nelson <nelson.guerreiro@nc-services.fr>
2023-08-04 07:56:22 +0200

    ff

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const userRouter = require('./routers/user');
-const checkIDRouter = require('./routers/checkID');
-const testUploadRouter = require('./routers/checkID');
-const itemRouter = require('./routers/item');
-const cartRouter = require('./routers/cart');
-const categoryRouter = require('./routers/category');
+// const userRouter = require('./routers/user');
+// const checkIDRouter = require('./routers/checkID');
+// const testUploadRouter = require('./routers/checkID');
+// const itemRouter = require('./routers/item');
+// const cartRouter = require('./routers/cart');
+// const categoryRouter = require('./routers/category');
 const mockRouter = require('./routers/mock');
 
 // const orderRouter = require("./routers/order");
@@ -33,11 +33,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(userRouter);
 
-app.use('/item', itemRouter);
-app.use('/testUpload', testUploadRouter);
-app.use('/cart', cartRouter);
-app.use('/checkId', checkIDRouter);
-app.use('/category', categoryRouter);
+// app.use('/item', itemRouter);
+// app.use('/testUpload', testUploadRouter);
+// app.use('/cart', cartRouter);
+// app.use('/checkId', checkIDRouter);
+// app.use('/category', categoryRouter);
 app.use('/mock', mockRouter);
 // app.use(orderRouter);
 module.exports = app;
