923c50db021f564d6123e96927331937c01ad0cd
Nelson <nelson.guerreiro@nc-services.fr>
2023-08-05 04:31:09 +0200

    tt

--- routers/category.js
+++ routers/category.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Category = require('../models/category');
+const Item = require('../models/item');
 
 const router = new express.Router();
 
@@ -15,6 +16,34 @@ router.get('/categories', async (req, res) => {
 	const categories = await Category.find();
 	console.log(categories);
 	res.json({ result: true, data: categories });
+});
+
+router.get('/categories/welcome', async (req, res) => {
+	try {
+		const categoriesWithItemCount = await Category.aggregate([
+			{
+				$lookup: {
+					from: 'items',
+					localField: '_id',
+					foreignField: 'category',
+					as: 'items',
+				},
+			},
+			{
+				$project: {
+					name: 1,
+					itemCount: { $size: '$items' },
+				},
+			},
+			{
+				$sort: { itemCount: -1 },
+			},
+		]);
+
+		res.json(categoriesWithItemCount);
+	} catch (err) {
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 });
 
 module.exports = router;
